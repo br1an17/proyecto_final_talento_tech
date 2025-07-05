3e11f01915cd39936d52217f8eafa3455502511f
br1an17 <brianzairpaez@gmail.com>
2025-07-05 16:27:53 -0300

    empezamos con el proyecto

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # proyecto_final_talento_tech
+
+Este es el proyecto final de Node.JS
+
+
---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("Hello, World!");
+// This is a simple Node.js script that prints "Hello, World!" to the console.
+// It serves as a basic example of how to run a Node.js application.    
